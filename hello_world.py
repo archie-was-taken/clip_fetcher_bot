a20e660373993c5f3de2f6c80ce556308b2fea4b
arch <113207560+archthegreat@users.noreply.github.com>
2023-12-08 20:31:44 +0530

    Refactor Telegram message handling and error handling

--- hello_world.py
+++ hello_world.py
@@ -6,7 +6,12 @@ import time
 
 # Third-party modules
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (ApplicationBuilder, 
+                          ContextTypes, 
+                          CommandHandler, 
+                          MessageHandler,
+                          filters
+                        )
 from telegram.constants import ParseMode
 
 with open('api_key.txt') as api_key:
@@ -41,14 +46,23 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def url(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = context.args[0]
 
-    output = subprocess.getoutput(
+    output = subprocess.getstatusoutput(
         f'yt-dlp --no-warnings -f best/bestvideo+bestaudio --get-url {url}')
-    
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=f'Click <a href="{output}">here</a> to download the video.',
-        parse_mode=ParseMode.HTML,
-        reply_to_message_id=update.message.message_id,)
+    if output[0] != 0:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Invalid URL. No media source found at target.'
+            ' Please try again.',
+            reply_to_message_id=update.message.message_id,
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'Click <a href="{output[1]}">here</a>' 
+            ' to download the video.',
+            parse_mode=ParseMode.HTML,
+            reply_to_message_id=update.message.message_id,)
+
 
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
@@ -61,13 +75,20 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
             chat_id=chat_id,
             text='Downloading video and sending. Please wait.',
             reply_to_message_id=update.message.message_id)
-        
-    subprocess.call(
-        ['yt-dlp', '--format', 'bestvideo+bestaudio/best', 
-        '--merge-output-format', 'mp4', '--concurrent-fragments', '4', 
-        '--no-playlist', f'{url}', '-o',f'{file_path}']
+    try:    
+        subprocess.run(
+            ['yt-dlp', '--format', 'bestvideo+bestaudio/best', 
+            '--merge-output-format', 'mp4', '--concurrent-fragments', '4', 
+            '--no-playlist', f'{url}', '-o',f'{file_path}'],
+            check = True
+            )
+    except subprocess.CalledProcessError:
+        await context.bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=temp_message.message_id,
+            text='Invalid URL. No media source found at target.'
+            ' Please try again.'
         )
-    
     # subprocess.call(['ffmpeg', '-i', file_path, '-vf', 'scale=1:360', 
     #                compressed_file_path])
     
@@ -87,26 +108,26 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
         os.remove(file_path)
     
 
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that! 😕 It seems like you've"
+        " sent an invalid command."
+        " See /help if you'd like to have a look at the available commands.",
+        reply_to_message_id=update.message.message_id,
+    )
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(api_key).build()
 
-    application.add_handler(
-        CommandHandler('start', start)
-    )
-    application.add_handler(
-        CommandHandler('url', url)
-    )
-    application.add_handler(
-        CommandHandler('download', download)
-    )
-    application.add_handler(
-        CommandHandler('dl', download)
-    )
-    application.add_handler(
-        CommandHandler('help', help_command)
-    )
-
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('url', url))
+    application.add_handler(CommandHandler('download', download))
+    application.add_handler(CommandHandler('dl', download))
+    application.add_handler(CommandHandler('help', help_command))
+    
+    # handler for unknown commands
+    application.add_handler(MessageHandler(filters.COMMAND, unknown))
     application.run_polling()
 
